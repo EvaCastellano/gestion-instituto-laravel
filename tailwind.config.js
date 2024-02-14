003f2438a0e4df4b4c90b58e98376925f6b41fbe
EvaCastellano <evacastellano60@gmail.com>
2024-02-14 10:32:29 +0100

    creacion archivo main.blade.php y cambios en tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,30 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
-            height:{
-                "10v":"10hv",
+            height: {
+                "10vh": "10vh", //1º nombre de la clase | 2º valor de la clase
+                "15vh": "15vh",
+                "65vh": "65vh",
             },
-            color:{
-
+            color: {
+                header: "#6915C2",
+                footer: "#1531BE",
+                main: "#9F15BE",
+                nav: "#2E15BE",
             },
-
         },
     },
 
